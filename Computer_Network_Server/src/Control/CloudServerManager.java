362db32bf469c1f85a7c2ceefeabe1eb9e6b7de6
HAN <gcg5@naver.com>
2018-11-06 03:48:07 +0900

    싱글톤 구현하기
    instance가 null인 경우에만 인자를 생성하고 생성자가 private이기 때문에 접근 불가

--- Computer_Network_Server/src/Control/CloudServerManager.java
+++ Computer_Network_Server/src/Control/CloudServerManager.java
@@ -1,5 +1,17 @@
 package Control;
 
+import java.net.InetAddress;
+
 public class CloudServerManager {
+
+    private static CloudServerManager instance;
+    private CloudServerManager(){}
+
+    public static CloudServerManager getInstance(){
+        if(instance==null)
+            instance=new CloudServerManager();
+        return instance;
+    }
+
 
 }
