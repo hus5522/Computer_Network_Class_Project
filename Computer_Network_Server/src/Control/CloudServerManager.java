cf52a53e1905377fd25e3d8841544e1663fe4eb7
Jo Huijae <whgmlwo1000@gmail.com>
2018-11-05 21:40:09 +0900

    Make CloudServerManager into Singleton Class

--- Computer_Network_Server/src/Control/CloudServerManager.java
+++ Computer_Network_Server/src/Control/CloudServerManager.java
@@ -1,5 +1,17 @@
 package Control;
 
 public class CloudServerManager {
+    private static CloudServerManager instance = null;
+
+    public CloudServerManager GetInstance()
+    {
+        if(instance == null) instance = new CloudServerManager();
+        return instance;
+    }
+
+    private CloudServerManager()
+    {
+
+    }
 
 }
